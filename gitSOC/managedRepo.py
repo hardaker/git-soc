f85146a420d8bdacfd3d26e0466ce3094e1db049
Wes Hardaker <opensource@hardakers.net>
2017-01-21 11:25:15 -0800

    let's try inheritence of git.Repo until it fails

--- gitSOC/managedRepo.py
+++ gitSOC/managedRepo.py
@@ -2,21 +2,17 @@
 
 import git
 
-class ManagedRepo(object):
+class ManagedRepo(git.Repo):
 
     def __init__(self, path):
         print "path: " + path
-        self._repo = git.Repo(path)
+        git.Repo.__init__(self, path)
         self._path = path
         
     def path(self):
         return self._path
 
-    def is_dirty(self):
-        return self._repo.is_dirty()
-
 if __name__ == "__main__":
     mr = ManagedRepo(".")
     print(mr)
-    print(mr.repo)
-    print "dirty: " + str(mr.repo.is_dirty())
+    print "dirty: " + str(mr.is_dirty())
