da76425366358818125348472b70d708299d9fee
Wes Hardaker <opensource@hardakers.net>
2017-01-24 08:11:02 -0800

    verbose mode

--- gitSOC/cmd/status.py
+++ gitSOC/cmd/status.py
@@ -11,14 +11,32 @@ class Status(gitSOC.cmd.Cmd):
     def __init__(self, soc, baseargs = {}):
         gitSOC.cmd.Cmd.__init__(self, soc, baseargs)
 
-    def print_repo_status(self, repo, otherargs = None):
+    def parse_args(self, args):
+        p = argparse.ArgumentParser()
+        p.add_argument("--verbose", "-v",
+                       action="store_true",
+                       help="Verbose mode")
+        parsed_args = p.parse_args(args = args)
+        return parsed_args
+
+    def print_repo_status(self, repo, args = None):
         state = " "
         if repo.is_dirty():
             state = "d"
 
         print("%-60s %s" % (repo.path(), state))
-
+        if args.verbose:
+            print("  %-10s: %s" % ("branch:", repo.active_branch))
+            print("  %-10s: %s" % ("head:", repo.head))
+            try:
+                remote = repo.remote()
+                if remote:
+                    print("  %-10s: %s" % ("remote:", remote.name))
+            except:
+                pass
+            
     def run(self, args):
-        self.soc.foreach_repo(self.print_repo_status)
+        print args
+        self.soc.foreach_repo(self.print_repo_status, args)
         
 
