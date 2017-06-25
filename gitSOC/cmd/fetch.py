99de5e8ca2c9b137104c4a6e639a023a93b68fd1
Wes Hardaker <opensource@hardakers.net>
2017-06-25 11:05:55 -0700

    fix fetching

--- gitSOC/cmd/fetch.py
+++ gitSOC/cmd/fetch.py
@@ -18,29 +18,29 @@ class Fetch(gitSOC.cmd.Cmd):
             print("%-60s %s" % (repo.path(), "needs clone"))
             return
 
-        try:
-            # do the actual fetch
-            remote = repo.remote()
-            self.verbose("fetching " + repo.path() + ":")
-            if remote:
-                oldcommit = remote.repo.commit()
-                x = remote.fetch()
-                newcommit = remote.repo.commit()
-                self.verbose("  fetch result: " + str(x))
-                self.verbose("  old: " + str(x[0].old_commit))
-                self.verbose("  new: " + str(x[0].commit))
-                self.verbose("  flags:" + str(x[0].flags))
-                if oldcommit != newcommit:
-                    result = str(oldcommit)[0:6] + ".." + str(newcommit)[0:6]
+        self.verbose("fetching " + repo.path() + ":")
+        for remote in repo.get_remotes():
+            try:
+                # do the actual fetch
+                if remote:
+                    oldcommit = repo.commit(remote['name'] + "/" + remote['branch'])
+                    x = repo.remote(remote['name']).fetch()
+                    newcommit = repo.commit(remote['name'] + "/" + remote['branch'])
+                    self.verbose("  fetch result: " + str(x))
+                    self.verbose("  old: " + str(x[0].old_commit))
+                    self.verbose("  new: " + str(x[0].commit))
+                    self.verbose("  flags:" + str(x[0].flags))
+                    if oldcommit != newcommit:
+                        result = str(oldcommit)[0:6] + ".." + str(newcommit)[0:6]
+                    else:
+                        result = "[up to date]"
                 else:
-                    result = "[up to date]"
-            else:
-                result = "no remote - weird bug"
-        except Exception as e:
-            print e
-            result = "failed"
-        except:
-            result = "fail - crashed"
+                    result = "no remote - weird bug"
+            except Exception as e:
+                print e
+                result = "failed"
+            except:
+                result = "fail - crashed"
             
         print("%-60s %s" % (repo.path(), result))
 
