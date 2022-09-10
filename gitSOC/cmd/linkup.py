1b0e1df7e384751263a99603275c8e7afb8083a5
Wes Hardaker <opensource@hardakers.net>
2022-09-10 05:10:16 -0700

    adding missing file

---
+++ gitSOC/cmd/linkup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# note: requirse python2-GitPython, not just "GitPython"
+
+import yaml
+import gitSOC.cmd
+import argparse
+import git
+import os
+
+class Linkup(gitSOC.cmd.Cmd):
+
+    def __init__(self, soc, baseargs = {}):
+        gitSOC.cmd.Cmd.__init__(self, soc, baseargs)
+
+    def linkup(self, repo, args):
+        repo.check_symlink(create = True)
+
+    def run(self, args):
+        self.soc.foreach_repo(self.linkup, args)
